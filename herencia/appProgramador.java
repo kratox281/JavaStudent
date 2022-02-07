715e04d0a35015d6b4ca58c4df4557a15fa36a96
ruben <rcmartinez281@gmail.com>
2022-02-07 19:27:56 +0100

    configuracion del portatil

--- herencia/appProgramador.java
+++ herencia/appProgramador.java
@@ -3,6 +3,6 @@ package herencia;
 public class appProgramador {
   public static void main(String[] args) {
       programador paco = new programador();
-      
+      System.out.println("hola paco");
   }  
 }
