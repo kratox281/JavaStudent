f88ec3caf1394c12a85a4854ef89e5758e7de4c0
Ruben <rcmartinez281@gmail.com>
2022-02-09 08:39:59 +0100

    "biblioteca"

--- Biblioteca/consulta.java
+++ Biblioteca/consulta.java
@@ -15,6 +15,26 @@ public class consulta {
     }
     public void setDocumento(){
         System.out.println("Que tipo de documento quieres consultar");
+        System.out.println("1:libro clasico,2:revista,3:digital,4:microfilms,5:genérico");
+        int n = tc.nextInt();
+        switch(n){
+            case 1:
+            this.consultado = new clasicos();
+            break;
+            case 2:
+            this.consultado = new revistas();
+            break;
+            case 3:
+            this.consultado = new digitales();
+            break;
+            case 4:
+            this.consultado = new microfilms();
+            break;
+            default:
+            this.consultado = new documento();
+            break;
+
+        }
     }
     public void setHoy(){
         Date fecha = new Date(System.currentTimeMillis());
