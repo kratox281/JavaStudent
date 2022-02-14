4161afa81f8d6428cba07aa6794d2fa7bf713ece
Ruben <rcmartinez281@gmail.com>
2022-02-14 12:39:03 +0100

    deleted

--- herencia/appProgramador.java
+++
@@ -1,8 +0,0 @@
-package herencia;
-
-public class appProgramador {
-  public static void main(String[] args) {
-      programador paco = new programador();
-      System.out.println("hola paco");
-  }  
-}
