4c6463ac8e322a4376df3308374842e7991324aa
kratox281 <rcmartinez281@gmail.com>
2022-03-28 13:50:49 +0200

    28/03/22

---
+++ LecturaEscritura/EjemploSelector.java
@@ -0,0 +1,11 @@
+package LecturaEscritura;
+
+import javax.swing.*;
+
+public class EjemploSelector {
+    public static void main(String[] args) {
+        JFileChooser jfc = new JFileChooser();
+        int resp = jfc.showOpenDialog(null);
+
+    }
+}
