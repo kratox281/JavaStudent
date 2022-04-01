b6a134220ce369365b4263fd1cb676d05ceeda97
kratox281 <rcmartinez281@gmail.com>
2022-04-01 13:54:02 +0200

    29/03/22

--- LecturaEscritura/Ejercicio1Files.java
+++ LecturaEscritura/Ejercicio1Files.java
@@ -13,23 +13,25 @@ public class Ejercicio1Files {
         int archivos  = 0;
         JFileChooser jfc = new JFileChooser();
         jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-    int resp = jfc.showSaveDialog(null);
+        int resp = jfc.showSaveDialog(null);
         if(resp == JFileChooser.APPROVE_OPTION){
 
             for(File f : jfc.getSelectedFile().listFiles()){
                 if(f.isDirectory()){
                     Date modi =new Date(f.lastModified()) ;
-                    System.out.println(modi+"<DIR>"+"       "+f.getName());
+                    System.out.println(modi+" <DIR> "+"     "+f.getName());
                 directorios++;
                 }
                 if(f.isFile()){
                     Date modi =new Date(f.lastModified()) ;
-                    System.out.println(modi+"     "+f.length()+f.getName());
+                    System.out.println(modi+"     "+f.length()+" "+f.getName());
                 archivos++;
                 }
             }
             System.out.println("Directorios: "+directorios);
             System.out.println("Archivos: "+archivos);
+        }else{
+            System.out.println("no se ha seleccionado nada");
         }
     }
 }
